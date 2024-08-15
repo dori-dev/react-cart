76af9d6a29e9fc552845cac690937ef6af7a1f79
dori-dev <mr.dori.dev@gmail.com>
2024-08-15 16:20:39 +0330

    Change product item button design

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import { CartContext } from "../context/CartContext";
@@ -18,27 +19,38 @@ function ProductItem({ product }) {
         <h5 className="card-title">{product.title}</h5>
         <p className="card-text">{product.price} Toman</p>
         {quantity > 0 ? (
-          <div className="row">
-            <div className="col-5">
+          <div className="row justify-content-between">
+            <div className="col-4">
               <button
                 type="button"
-                onClick={() => cart.removeItemFromCart(product.id)}
-                className="btn btn-sm btn-secondary w-100"
+                onClick={() => cart.deleteFromCart(product.id)}
+                className="btn btn-sm btn-danger w-100"
               >
-                -
+                <FaTrash></FaTrash>
               </button>
             </div>
-            <div className="col-2 text-center align-self-center">
-              <span className="w-100">{quantity}</span>
-            </div>
-            <div className="col-5">
-              <button
-                type="button"
-                onClick={() => cart.addItemToCart(product.id)}
-                className="btn btn-sm btn-secondary w-100"
-              >
-                +
-              </button>
+            <div className="row justify-content-center col-8">
+              <div className="col-4">
+                <button
+                  type="button"
+                  onClick={() => cart.removeItemFromCart(product.id)}
+                  className="btn btn-sm btn-secondary w-100"
+                >
+                  -
+                </button>
+              </div>
+              <div className="col-2 me-2 text-center align-self-center">
+                {quantity}
+              </div>
+              <div className="col-4">
+                <button
+                  type="button"
+                  onClick={() => cart.addItemToCart(product.id)}
+                  className="btn btn-sm btn-secondary w-100"
+                >
+                  +
+                </button>
+              </div>
             </div>
           </div>
         ) : (
