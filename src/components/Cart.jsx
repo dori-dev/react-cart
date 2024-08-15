6d8902ddf4bac563e07947638cf137dda31a23d9
dori-dev <mr.dori.dev@gmail.com>
2024-08-15 11:29:56 +0330

    Add cart modal

---
+++ src/components/Cart.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+function Cart({ show, close }) {
+  return (
+    <>
+      {show ? (
+        <div
+          className="modal fade show"
+          tabIndex={-1}
+          style={{ display: "block" }}
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header bg-dark">
+                <h1 className="modal-title fs-5" id="exampleModalLiveLabel">
+                  Cart
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close  btn-close-white"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  onClick={close}
+                />
+              </div>
+              <div className="modal-body bg-dark">
+                <p>The Product one, price 44, count 8</p>
+                <p>The Product two, price 44, count 3</p>
+                <p>The Product three, price 44, count 1</p>
+              </div>
+              <div className="modal-footer bg-dark row">
+                <button
+                  onClick={close}
+                  type="button"
+                  className="btn btn-secondary col-4"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary col-7">
+                  Continue
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </>
+  );
+}
+
+export default Cart;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsCart } from "react-icons/bs";
 
+import Cart from "./Cart";
+
 function Navbar() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleShow = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  console.log(showModal);
+
   return (
-    <nav className="navbar light-nav navbar-expand-lg bg-body-tertiary mb-5">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Shopping
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link align-self-center" href="#">
-                <BsCart className="mx-1 mb-1"></BsCart>
-                Cart
-              </a>
-            </li>
-          </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
+    <>
+      <nav className="navbar light-nav navbar-expand-lg bg-body-tertiary mb-5">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Shopping
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <button
+                  onClick={handleShow}
+                  type="button"
+                  className="nav-link align-self-center"
+                >
+                  <BsCart className="mx-1 mb-1"></BsCart>
+                  Cart
+                </button>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      <Cart show={showModal} close={handleClose} />
+    </>
   );
 }
 
